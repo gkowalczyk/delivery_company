81f02a57a76a066d4d6eb0b6f48a09b97bb2e6e6
Grzegorz Kowalczyk <grzegorzkowalczyk1989@gmail.com>
2023-05-01 14:29:43 +0200

    fix

---
+++ delivery/infrastructures/src/main/java/com/example/infrastructures/InfrastructuresApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastructuresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrastructuresApplication.class, args);
+    }
+
+}
---
+++ delivery/infrastructures/src/test/java/com/example/infrastructures/InfrastructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infrastructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructuresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
